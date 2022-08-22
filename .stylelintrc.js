139de9141e6d44b4946087359adaeef50219953d
Artem Tebenkov <artem.tebenkov@icloud.com>
2022-08-22 09:51:16 +0400

    feat: add detail section

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,9 +1,15 @@
 module.exports = {
   extends: [
-    "stylelint-config-standard",
-    "stylelint-config-rational-order",
-    "stylelint-prettier/recommended",
+    'stylelint-config-standard',
+    'stylelint-config-rational-order',
+    'stylelint-prettier/recommended',
   ],
-  plugins: ["stylelint-order", "stylelint-scss"],
-  rules: {},
+  plugins: ['stylelint-order', 'stylelint-scss'],
+  rules: {
+    'selector-class-pattern': null,
+    'keyframes-name-pattern': null,
+    'at-rule-no-unknown': null,
+    'no-empty-source': null,
+    'scss/at-rule-no-unknown': true,
+  },
 };
